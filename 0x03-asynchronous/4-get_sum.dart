d283af7703c06d75025a92c5f4cabd843e81e33d
Gabriel <gabyrivm06@gmail.com>
2025-12-14 19:57:09 -0400

    feat: Calcul Total

---
+++ 0x03-asynchronous/4-get_sum.dart
@@ -0,0 +1,43 @@
+import 'dart:convert';
+import '4-util.dart';
+
+Future<double> calculateTotal() async {
+  try {
+    final userDataJson = await fetchUserData();
+    final userData = jsonDecode(userDataJson);
+    if (userData is! Map) {
+      return -1;
+    }
+
+    final id = userData['id'];
+    if (id is! String) {
+      return -1;
+    }
+
+    final ordersJson = await fetchUserOrders(id);
+    final orders = jsonDecode(ordersJson);
+    if (orders is! List) {
+      return -1;
+    }
+
+    double total = 0;
+    for (final product in orders) {
+      if (product is! String) {
+        return -1;
+      }
+
+      final priceJson = await fetchProductPrice(product);
+      final decodedPrice = jsonDecode(priceJson);
+      if (decodedPrice is! num) {
+        return -1;
+      }
+      total += decodedPrice.toDouble();
+    }
+
+    return total;
+  } catch (error) {
+    return -1;
+  }
+}
+
+

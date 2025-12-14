f72752b021e88f6d460d579d74efd0b89316b61c
Gabriel <gabyrivm06@gmail.com>
2025-12-14 19:21:49 -0400

    feat: Private Password

---
+++ 0x02-dart-classes/3-private_password.dart
@@ -0,0 +1,21 @@
+class Password {
+    String _password;
+
+    Password({String password = ''}) : _password = password;
+
+    bool isValid() {
+        if (_password.length < 8 || _password.length > 16) {
+            return false;
+        }
+        if (!_password.contains(RegExp(r'[A-Z]')) ||
+            !_password.contains(RegExp(r'[a-z]')) ||
+            !_password.contains(RegExp(r'[0-9]'))) {
+            return false;
+        }
+        return true;
+    }
+
+    String toString() {
+        return 'Your Password is: ${this._password}';
+    }
+}

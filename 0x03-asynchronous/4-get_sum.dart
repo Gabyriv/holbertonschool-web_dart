d503514fa22b027bf5ded70d08a73c03c1c3c740
Gabriel <gabyrivm06@gmail.com>
2025-12-14 20:07:42 -0400

    feat: Calcul Total

--- 0x03-asynchronous/4-get_sum.dart
+++ 0x03-asynchronous/4-get_sum.dart
@@ -2,41 +2,18 @@ import 'dart:convert';
 import '4-util.dart';
 
 Future<double> calculateTotal() async {
-  try {
-    final userDataJson = await fetchUserData();
-    final userData = jsonDecode(userDataJson);
-    if (userData is! Map) {
-      return -1;
-    }
-
-    final id = userData['id'];
-    if (id is! String) {
-      return -1;
-    }
-
-    final ordersJson = await fetchUserOrders(id);
-    final orders = jsonDecode(ordersJson);
-    if (orders is! List) {
-      return -1;
-    }
-
-    double total = 0;
-    for (final product in orders) {
-      if (product is! String) {
+    try {
+        double price = 0;
+	
+        final Map<String, dynamic> userData = json.decode(await fetchUserData());
+        final String data = userData['id'];
+        final List<dynamic> userOrder = json.decode(await fetchUserOrders(data));
+        for (int i = 0; i < userOrder.length; i++) {
+            price += json.decode(await fetchProductPrice(userOrder[i]));
+        }
+        return price;
+    } catch (error) {
+        print('error caught: $error');
         return -1;
-      }
-
-      final priceJson = await fetchProductPrice(product);
-      final decodedPrice = jsonDecode(priceJson);
-      if (decodedPrice is! num) {
-        return -1;
-      }
-      total += decodedPrice.toDouble();
     }
-
-    return total;
-  } catch (error) {
-    print('error caught: $error');
-    return -1;
-  }
 }
